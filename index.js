e49b524db36f315d06dce4cbcf78123acf100e29
Aditya Agarwal <99405991+QMiNt@users.noreply.github.com>
2022-05-28 22:03:26 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var form = document.getElementById("f1");
+form.addEventListener('submit',function(e)
+{
+    e.preventDefault();
+    document.getElementById("err").style.display="none";
+    document.getElementById("res").style.display="none";
+    serch=document.getElementById("username").value;
+    fetch("https://api.github.com/users/" + serch)
+    .then((result)=>{
+        if(result.ok)
+        {
+            document.getElementById("res").style.display="block";
+            return result.json();
+        }
+        else
+        {
+            document.getElementById("err").style.display="inline";
+            document.getElementById("err").innerHTML="User Not Found";
+        }
+    })
+    .then((data) => {
+        console.log(data);
+        document.getElementById("name").innerHTML=data.name;       
+        document.getElementById("email").innerHTML=data.email;       
+        document.getElementById("bio").innerHTML=data.bio;       
+        document.getElementById("com").innerHTML=data.company;       
+    })
+})
